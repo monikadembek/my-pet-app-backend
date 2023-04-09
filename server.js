c1e1fa5d9d2b3ae230e44c7c409557b14a30f700
Monika Dembek <monika.dembek@knowit.eu>
2023-04-09 19:32:35 +0200

    Initial setup

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename); 
+const viewsPath = __dirname + '/src/views';
+
+const corsOptions = {
+  origin: 'http://localhost:4200'
+}
+
+app.use(cors(corsOptions));
+app.use(express.static(viewsPath));
+app.use(express.json());
+
+app.get('', (req, res) => {
+  res.sendFile(viewsPath + 'index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+process.on('uncaughtException', (err) => {
+  console.log('uncaught exception');
+  console.error(err);
+});
